cd4d249648823bcc91600d2135e95514bd58234a
Robert Dougan <rdougan@me.com>
2014-12-08 15:48:30 +0100

    Add menu button to tabbar

--- app.js
+++ app.js
@@ -67,6 +67,15 @@ Ext.application({
             side: 'left',
             reveal: true
         });
+
+        // Add button to navbar
+        var tabBar = Ext.Viewport.query('tabbar')[0];
+        tabBar.insert(0, {
+            iconCls: 'search',
+            handler: function() {
+                Ext.Viewport.toggleMenu('left');
+            }
+        });
     }, // End launch
    
     onUpdated: function () {
