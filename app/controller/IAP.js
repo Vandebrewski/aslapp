55d4508ff3cc40511499f2295cf5e7e9c04de7ad
Robert Dougan <rdougan@me.com>
2015-03-12 16:05:13 +0100

    moving IAP to the correct place

---
+++ app/controller/IAP.js
@@ -0,0 +1,60 @@
+Ext.define('ASLKids.controller.IAP', {
+     extend: 'Ext.app.Controller',
+
+     config: {
+          list: ['adfree']
+     },
+
+	init: function() {
+          // Check availability of the storekit plugin
+          if (!window.storekit) {
+               console.log("In-App Purchases not available");
+               return;
+          }
+      
+          // Initialize
+          storekit.init({
+               debug:    true, // Enable IAP messages on the console
+               ready:    this.onReady,
+               purchase: this.onPurchase,
+               restore:  this.onRestore,
+               error:    this.onError
+          });
+	},
+	
+	onReady: function() {
+          storekit.load(IAP.list, function (products, invalidIds) {
+               IAP.products = products;
+               IAP.loaded = true;
+               for (var i = 0; i < invalidIds.length; ++i) {
+                    console.log("Error: could not load " + invalidIds[i]);
+               }
+          });
+	},
+	
+	onPurchase: function(transactionId, productId, receipt) {
+          if(productId === 'adfree'){
+               alert("Ads Removed!");
+               //Code to remove ads for the user
+          }
+	},
+	
+	onRestore: function(transactionId, productId, transactionReceipt) {
+          if(productId == 'adfree'){
+               //Code to remove ads for the user
+          }
+	},
+	
+	onError: function(errorCode, errorMessage) {
+          console.log(errorCode);
+          console.log(errorMessage);
+	},
+	
+	buy: function(productId){
+          storekit.purchase(productId);
+	},
+	
+	restore: function(){
+          storekit.restore();
+	}
+});
