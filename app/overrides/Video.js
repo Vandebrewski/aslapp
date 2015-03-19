84165807a8d00a8d4434c75d5d329dd818053958
Vandebrewski <Bastiaan@MacBook-Pro-van-Bastiaan.local>
2015-03-19 17:48:34 +0100

    trying to override

--- app/overrides/Video.js
+++ app/overrides/Video.js
@@ -10,11 +10,14 @@ Ext.define('ASLKids.overrides.Video', {
     	},
     
         onPlay: function() {
-        this.callParent(arguments); // can this be optimized to this.callParent([ parentNode, index ]); http://www.sencha.com/blog/top-support-tips-august-2014/
+//        this.callParent(arguments); // can this be optimized to this.callParent([ parentNode, index ]); http://www.sencha.com/blog/top-support-tips-august-2014/
         this.media.setBottom(0);
         console.log('my override is working onPlay', arguments); // I can see this message on play so this works        
-    	}
+    	},
     
+    	onPlay: function() {
+        this.fireEvent('play', this);
+    },
    
 // onGhostTap: I think their should be a delay in showing the video (/removing the poster outside the screen) to avoid the black flash.
    
